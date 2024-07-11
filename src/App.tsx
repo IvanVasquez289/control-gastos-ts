7ed76f8e03b51b640e46d2dbc253040d8bc577ca
ivan <ivanjv1234@gmail.com>
2024-07-11 15:13:16 -0600

    feat: validar presupuesto

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import BudgetForm from "./components/BudgetForm"
+
 function App() {
 
   return (
     <>
-      <h1>Hola mundo</h1>
+      <header className="bg-blue-600 py-10 max-h-72">
+        <h1 className="uppercase text-4xl text-white font-black text-center">
+          Planificador de Gastos
+        </h1>
+      </header>
+      <div className="max-w-3xl mx-auto shadow-lg rounded-lg mt-10 p-10 bg-white">
+        <BudgetForm/>
+      </div>
     </>
   )
 }
---
+++ src/components/BudgetForm.tsx
@@ -0,0 +1,43 @@
+import { ChangeEvent, useMemo, useState } from "react";
+
+const BudgetForm = () => {
+  const [budget, setBudget] = useState(0);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setBudget(e.target.valueAsNumber);
+  };
+
+  const isValid = useMemo(() => {
+    return (budget <= 0 || isNaN(budget)) ? false : true   
+  }, [budget])
+
+  return (
+    <form className="space-y-5">
+      <div className="flex flex-col space-y-5">
+        <label
+          htmlFor="budget"
+          className="text-blue-600 text-4xl font-bold text-center"
+        >
+          Definir presupuesto
+        </label>
+        <input
+          type="number"
+          name="budget"
+          id="budget"
+          placeholder="Definir presupuesto"
+          className="border-2 border-gray-300 bg-white  p-3  w-full"
+          value={budget}
+          onChange={handleChange}
+        />
+      </div>
+      <input 
+        type="submit" 
+        value="Definir presupuesto" 
+        className="disabled:opacity-45 disabled:cursor-not-allowed bg-blue-600 hover:bg-blue-700 cursor-pointer uppercase w-full p-3 text-white font-black"
+        disabled={!isValid}
+       />
+    </form>
+  );
+};
+
+export default BudgetForm;
