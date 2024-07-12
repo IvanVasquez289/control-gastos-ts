961756f4794517416932df90bdbdd9bf1e53b9d2
ivan <ivanjv1234@gmail.com>
2024-07-11 20:45:23 -0600

    feat: componente de grafica

---
+++ src/components/AmountDisplay.tsx
@@ -0,0 +1,18 @@
+import { formatCurrency } from "../helpers"
+
+type AmountDisplayProps = {
+    label: string
+    amount: number
+}
+const AmountDisplay = ({label, amount}: AmountDisplayProps) => {
+  return (
+    <div>
+        <p className="text-blue-600 text-2xl font-bold">
+          {label}: {' '}  
+          <span className="font-black text-black">{formatCurrency(amount)}</span>
+        </p>
+    </div>
+  )
+}
+
+export default AmountDisplay
--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,8 +1,34 @@
+import AmountDisplay from "./AmountDisplay";
 
 const BudgetTracker = () => {
   return (
-    <div>BudgetTracker</div>
-  )
-}
+    <div className="grid grid-cols-1 md:grid-cols-2">
+      <div className="flex justify-center">
+        <img src="/grafico.jpg" alt="" />
+      </div>
+      <div className="flex flex-col justify-center items-center gap-8 ">
+        <button
+          type="button"
+          className="bg-pink-600 uppercase font-bold text-white w-full p-2 rounded-lg"
+        >
+          Resetear App
+        </button>
 
-export default BudgetTracker
+        <AmountDisplay
+          label="Presupuesto"
+          amount={500}
+        />
+        <AmountDisplay
+          label="Disponible"
+          amount={300}
+        />
+        <AmountDisplay
+          label="Gastado"
+          amount={200}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default BudgetTracker;
