644b53ae6008b34a82af055b49cd62219286bc59
ivan <ivanjv1234@gmail.com>
2024-07-28 01:18:19 -0600

    feat: grafica circular

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,13 +1,23 @@
 import AmountDisplay from "./AmountDisplay";
 import { useBudget } from "../hooks/useBudget";
-
+import { CircularProgressbar , buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 const BudgetTracker = () => {
   const {state,totalExpenses,remainingBudget} = useBudget()
-  
+  const percentage = +(totalExpenses / state.budget * 100).toFixed(2);
   return (
     <div className="grid grid-cols-1 md:grid-cols-2">
       <div className="flex justify-center">
-        <img src="/grafico.jpg" alt="" />
+        <CircularProgressbar
+          value={percentage}
+          text={`${percentage}% Gastado`}
+          styles={buildStyles({
+            textColor: percentage === 100 ? "#DC2626" : "#3b82f6",
+            textSize: 8,
+            pathColor: percentage === 100 ? "#DC2626" : "#3b82f6",
+            trailColor: "#f5f5f5",
+          })}
+        />
       </div>
       <div className="flex flex-col justify-center items-center gap-8 ">
         <button
